0ade566a78fded06263bdaa1ab1d1ba50fa082bb
Tyler Cook <tyler.b.cook4@gmail.com>
2018-10-04 16:36:36 -0500

    Added better command parsing

--- instruction.cpp
+++ instruction.cpp
@@ -22,50 +22,77 @@ bool Instruction::load(string commandStr)
 {
     stringstream ss; // Stringstream used to separate values
 
-    cout << "Command str is : " << commandStr << endl;
-
     // Load the stream with the line read from input
     ss << commandStr;
     ss >> command;
 
-    // Convert to uppercase
-    to_upper(command
-             );
+    // Convert the command to uppercase
+    to_upper(command);
 
     // Check and perform command as given
     // Add Subject
-    if (command.compare("addsub"))
+    if (command == "ADDSUB")
     {
         ss >> subject_name >> security_level;
+        to_upper(security_level);
 
-        // Verify command is valid
-
-
-
+        // Verify security level valid and stream is empty
+        if ((!(security_level == "LOW" || security_level == "MEDIUM" || security_level == "HIGH")) || (!ss.eof()))
+        {
+            cout << "Bad Command: " << commandStr << endl;
+            return false;
+        }
 
         cout << "Subject Added: " << command << " " << subject_name << " " << security_level << endl;
     }
 
     // Add Object
-    else if (command.compare("addobj"))
+    else if (command == "ADDOBJ")
     {
         ss >> object_name >> security_level;
+        to_upper(security_level);
+
+        // Verify security level valid and stream is empty
+        if ((!(security_level == "LOW" || security_level == "MEDIUM" || security_level == "HIGH")) || (!ss.eof()))
+        {
+            cout << "Bad Command: " << commandStr << endl;
+            return false;
+        }
+
         cout << "Object Added: " << command << " " << object_name << " " << security_level << endl;
     }
 
     // Read an object
-    else if (command.compare("read"))
+    else if (command == "read")
     {
-        cout << "In read\n";
         ss >> subject_name >> object_name;
-        cout << "Object Added: " << command << " " << object_name << " " << security_level << endl;
+
+        // Make sure nothing left in stream
+        if (!ss.eof())
+        {
+            cout << "Bad Command: " << commandStr << endl;
+            return false;
+        }
+
+        cout << "Read: " << command << " " << subject_name << " " << object_name << endl;
     }
 
     // Write to an object
-    else if (command.compare("write"))
+    else if (command == "write")
     {
         ss >> subject_name >> object_name >> value;
+
+        // Make sure nothing left in stream
+        if (!ss.eof())
+        {
+            cout << "Bad Command: " << commandStr << endl;
+            return false;
+        }
+
+        cout << "Write: " << command << " " << subject_name << " " << object_name << endl;
     }
+
+    // If we get here, we have a bad command
     else
     {
         cout << "Bad Command: " << commandStr << endl;
@@ -76,12 +103,11 @@ bool Instruction::load(string commandStr)
 }
 
 // Convert string to uppercase
-// TODO: might not need this
 void Instruction::to_upper(string &str)
 {
     for (int i = 0; str[i] != 0; i++)
     {
-        if(str[i] <= 'z' && str[i] >= 'a')
+        if(str[i] >= 'a' && str[i] <= 'z')
         {
             str[i]-=32;
         }
